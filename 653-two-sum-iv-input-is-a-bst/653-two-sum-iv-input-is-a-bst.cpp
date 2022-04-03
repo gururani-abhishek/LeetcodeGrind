f706b63984be750389e953280fb25b3edb97d86e
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-03 23:54:31 +0530

    Time: 44 ms (78.24%), Space: 36.7 MB (89.30%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -9,47 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class BSTIterator {
-public: 
-    stack<TreeNode*> stk;
-    bool who; //true for left, and false for right;
-    
-    BSTIterator(TreeNode* root, bool which) {
-        who = which;
-        pushAll(root);
+class Solution {
+public:
+    stack<TreeNode*> stk, stk2;
+    bool findTarget(TreeNode* root, int k) {
+        pushLeftAll(root);
+        pushRightAll(root);
+        int i = outFront(), j = outBack();
+        
+        while(i < j) {
+            if(i + j ==  k) return true;
+            else if(i + j < k) i = outFront();
+            else j =  outBack();
+        }
+        
+        return false;
     }
     
-    int next() {
-        TreeNode* node = stk.top();
-        stk.pop();
-        who ? pushAll(node -> right) : pushAll(node -> left);
+    int outBack() {
+        TreeNode* node = stk2.top();
+        stk2.pop();
+        pushRightAll(node -> left);
         return node -> val;
     }
     
-private:
-    void pushAll(TreeNode* node) {
-        while(node != NULL) {
-            stk.push(node);
-            who ? node = node -> left : node = node -> right;
+    int outFront()  {
+        TreeNode* node = stk.top();
+        stk.pop();
+        pushLeftAll(node -> right);
+        return node -> val;
+    }
+    
+    void pushLeftAll(TreeNode* root) {
+        while(root != NULL) {
+            stk.push(root);
+            root = root -> left;
         }
     }
     
-};
-
-class Solution {
-public:
-    bool findTarget(TreeNode* root, int k) {
-        if(root == NULL) return false;
-        BSTIterator l(root, true);
-        BSTIterator r(root, false);
-        
-        int i = l.next(), j = r.next();
-        
-        while(i < j) {
-            if(i + j == k)  return true;
-            else if( i + j < k) i = l.next();
-            else j = r.next();
+    void pushRightAll(TreeNode* root) {
+        while(root != NULL) {
+            stk2.push(root);
+            root = root -> right;
         }
-    return false;
     }
 };
