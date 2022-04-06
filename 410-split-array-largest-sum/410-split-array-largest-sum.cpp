967ada1106aa98f2c3787e0ab8db8154423c25d1
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-06 09:06:31 +0530

    Time: 4 ms (53.48%), Space: 7.2 MB (73.78%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-    
     int splitArray(vector<int>& nums, int m) {
-        int l =-1e9, h =0;
-        for(auto u: nums) {
-            h += u;
+        int l = -1e9, h = 0;
+        for(auto u: nums)  {
             l = max(l, u);
+            h += u;
         }
         
         while(l < h) {
-            int mid = l + (h - l)/2;
-            if(countSubarrays(nums, mid) <= m) {
-                h = mid;
-            } else {
-                l = mid + 1;
-            }
+            int mid = l + (h -l)/2;
+    
+            if(countSubarrays(nums, mid) <= m) h = mid; 
+            else l = mid + 1;
         }
-        
         
         return l;
     }
-
+    
 private: 
-    int countSubarrays(vector<int> nums, int maxS) {
+    int countSubarrays(vector<int>& nums, int sum) {
         int total = 0, subarrays = 1;
-        
-        
         for(int i=0; i<nums.size(); i++) {
-            if(total + nums[i] <= maxS) total += nums[i];
+            if(total + nums[i] <= sum) total += nums[i];
             else {
                 total = nums[i];
                 subarrays++;
             }
         }
-        
         return subarrays;
     }
 };
