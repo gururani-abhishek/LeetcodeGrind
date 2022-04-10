c06357a8418deca029fa286e90c79b3a1b34410a
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-10 13:55:10 +0530

    Time: 4 ms (52.78%), Space: 7.2 MB (48.33%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    set<vector<int>> set;
-    void fun(vector<int> element, vector<int> nums) {
-        if(nums.size() == 0) {
-            set.insert(element);
-            return;
-        }
-        
-        vector<int> element1 = element;
-        vector<int> element2 = element;
-        
-        element2.push_back(nums[0]);
-        nums.erase(nums.begin()+ 0);
-        
-        fun(element1, nums);
-        fun(element2, nums);
-    }
-    
     
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> element;  
-    
-        fun(element, nums);
+        int n = nums.size();
         
-        for(auto u: set) {
-            ans.push_back(u);
+        
+        for(int i=0; i<(1<<n); i++) {
+            vector<int> element;
+            for(int j = 0; j<n; j++) {
+                if(i&(1<<j)) element.push_back(nums[j]);
+            }
+            ans.push_back(element);
         }
         
         return ans;
-
+        
     }
 };
