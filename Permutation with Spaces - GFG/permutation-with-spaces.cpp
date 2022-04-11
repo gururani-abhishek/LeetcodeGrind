05dda275db7c210a469f57c02b8c450424ec6849
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-12 00:09:24 +0530

    Added solution - LeetHub

--- Permutation with Spaces - GFG/permutation-with-spaces.cpp
+++ Permutation with Spaces - GFG/permutation-with-spaces.cpp
@@ -7,35 +7,31 @@ using namespace std;
 class Solution{
 public:
     vector<string> ans;
-    
-    void createPermutations(string op, string ip) {
-        if(ip.length() == 0) {
+    vector<string> permutation(string S){
+        string op = "";
+        op.push_back(S[0]);
+        S.erase(S.begin() + 0);
+        create(op, S);
+        return ans;
+    }
+private: 
+    void create(string op, string ip) {
+        if(ip.size() == 0) {
             ans.push_back(op);
             return;
         }
         
         string op1 = op;
+        string op2 = op;
         
-        //op1 with spaceChar, op2 without spaceChar
         op1.push_back(' ');
         op1.push_back(ip[0]);
-        op.push_back(ip[0]);
+        
+        op2.push_back(ip[0]);
         
         ip.erase(ip.begin() + 0);
-        
-        createPermutations(op1, ip);
-        createPermutations(op, ip);
-        
-    }
-    
-    vector<string> permutation(string S){
-        string op = "";
-        op.push_back(S[0]);
-        S.erase(S.begin() + 0);
-        
-        createPermutations(op, S);
-        
-        return ans;
+        create(op1, ip);
+        create(op2, ip);
     }
 };
 
