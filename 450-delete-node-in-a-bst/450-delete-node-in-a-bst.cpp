a57e498dae1701e34646d6115402a9df46107f70
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-12 20:18:57 +0530

    Time: 39 ms (73.11%), Space: 32.7 MB (76.54%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -14,47 +14,51 @@ public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root == NULL) return NULL;
         
-        if(root -> val == key) return helper(root);
+        if(root -> val == key) return reConnect(root);
         
         TreeNode* cur = root;
-        
         while(cur != NULL) {
-             if(cur -> val > key) {
+            // if(cur -> val == key) {
+            //     cur = reConnect(cur);
+            //     break;
+            // }else if(cur -> val < key) cur = cur -> right;
+            // else cur = cur -> left; 
+            //I was sending that node instead of the parent node, was messing up pointers.
+            if(cur -> val > key) {
                 if(cur -> left != NULL && cur -> left -> val == key) {
-                    cur -> left = helper(cur -> left);
+                    cur -> left = reConnect(cur -> left);
                     break;
                 } else {
                     cur = cur -> left;
-                }      
-             } else {
-                 if(cur -> right != NULL && cur -> right -> val == key) {
-                     cur -> right = helper(cur -> right);
-                     break;
-                 } else {
-                     cur = cur -> right;
-                 }
-             }
+                }
+            } else {
+                if(cur -> right != NULL && cur -> right -> val == key) {
+                    cur -> right = reConnect(cur -> right);
+                    break;
+                } else {
+                    cur = cur -> right;
+                }
+            }
+            
         }
         
         return root;
-        
     }
-    
-    TreeNode* helper(TreeNode* node) {
-        if(node -> left == NULL) return node -> right;
+private: 
+    TreeNode* reConnect(TreeNode* node) {
+        //if it has only 1 child
         if(node -> right == NULL) return node -> left;
+        if(node -> left == NULL) return node -> right;
+            
+        //if it has 2 children
+        TreeNode* point = findLeftMost(node -> right);
+        point -> left = node -> left;
         
-        //both the children exist.
-        TreeNode* point = findRightMost(node -> left);
-        point -> right = node -> right;
-        return node -> left;
+        return node -> right;
     }
     
-    TreeNode* findRightMost(TreeNode* node) {
-        if(node -> right == NULL) return node;
-        
-        return findRightMost(node -> right);
-    }
-    
-    
+   TreeNode* findLeftMost(TreeNode* node) {
+       while(node -> left != NULL) node = node -> left;
+       return node;
+   }
 };
