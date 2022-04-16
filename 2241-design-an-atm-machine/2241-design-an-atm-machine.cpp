9adcd9a2a45c66775912bc5727113fd13eb5e093
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-17 02:12:03 +0530

    Time: 656 ms (100.00%), Space: 107.9 MB (66.67%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
@@ -1,33 +1,46 @@
 class ATM {
 public:
-    map<int, long long int, greater<int>> freq;
+    //map<int, long long int, greater<int>> freq;
+    vector<int> deno{20, 50, 100, 200, 500};
+    vector<long long int> freq;
     ATM() {
-        
+        freq.resize(5);
     }
     
     void deposit(vector<int> banknotesCount) {
-        freq[20] += banknotesCount[0];
-        freq[50] += banknotesCount[1];
-        freq[100] += banknotesCount[2];
-        freq[200] += banknotesCount[3];
-        freq[500] += banknotesCount[4];
+        // freq[20] += banknotesCount[0];
+        // freq[50] += banknotesCount[1];
+        // freq[100] += banknotesCount[2];
+        // freq[200] += banknotesCount[3];
+        // freq[500] += banknotesCount[4];
+        
+        for(int i = 0; i<5; i++) freq[i] += banknotesCount[i];
+        
     }
     
     vector<int> withdraw(int amount) {
-        map<int, long long int, greater<int>> freq1 = freq;
-        vector<int> ans;
+        //map<int, long long int, greater<int>> freq1 = freq;
+        vector<int> ans(5);
         
-        for(auto &u: freq1) {
-            long long int x = min((long long int) amount / u.first, u.second);
-            amount -= x * u.first;
-            u.second -= x;
-            ans.push_back(x);
+        // for(auto &u: freq1) {
+        //     long long int x = min((long long int) amount / u.first, u.second);
+        //     amount -= x * u.first;
+        //     u.second -= x;
+        //     ans.push_back(x);
+        // }
+        
+        for(int i =4; i>=0; i--) {
+            long long int x = min((long long int)  amount/deno[i], freq[i]);
+            amount -= x * deno[i];
+            ans[i] = x;
         }
         
-        if(amount) return {-1};
+        if(amount != 0) return {-1};
         
-        freq = freq1;
-        reverse(ans.begin(), ans.end());
+        for(int i=0; i<5; i++) 
+            freq[i] -= ans[i];
+        
+        //reverse(ans.begin(), ans.end());
         return ans;
     }
 };
