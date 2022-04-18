f459a5599c0c04de9666a7ae95fbc0f197b565d2
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-18 12:37:26 +0530

    Time: 43 ms (61.71%), Space: 12.9 MB (99.49%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int, vector<int>, greater<int>> minHeap;
         int n = matrix.size();
+        int l = matrix[0][0], h = matrix[n-1][n-1];
         
-        for(int i=0; i<n; i++) { 
-            for(int j=0; j<n; j++) {
-                minHeap.push(matrix[i][j]);
-                if(minHeap.size() > n*n-k+1) minHeap.pop();
-                
-            }
+        while(l < h) {
+            int mid = l + (h -l)/2;
+            if(countLessOrEqual(matrix, mid) >= k) h = mid;
+            else l = mid + 1;
         }
-        
-        return minHeap.top();
+
+        return l;
+    }
+private: 
+    int countLessOrEqual(vector<vector<int>>& matrix, int key) {
+        int count = 0, n = matrix.size();
+        //int j = n-1;
+        for(int i=0; i<n; i++) {
+            // while(j>=0 && matrix[i][j] >key) j--;
+            // count += (j+ 1);
+            for(int j = 0; j<n && matrix[i][j] <= key; j++) count++;
+        }
+        return count;
     }
 };
-
-//tc: O(n*n*log(n-k+1)) //building a heap of size 'n-k+1'
-//sc: O(n-k+1) //heap of size k
