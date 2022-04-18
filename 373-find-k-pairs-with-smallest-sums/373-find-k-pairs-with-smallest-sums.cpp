3fa9275c54aa94a761c7d18dafbb525d7206fcd0
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-18 14:27:16 +0530

    Time: 394 ms (54.57%), Space: 109.6 MB (74.22%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int n = nums1.size(), m = nums2.size();
         priority_queue<pair<int, pair<int, int>>> maxHeap;
-        //create pairs
-        for(int i=0; i<n; i++) {
-            for(int j= 0; j<m; j++) {
-                int sum = nums1[i] + nums2[j];
+        
+        for(int i=0; i<nums1.size(); i++) {
+            for(int j = 0; j<nums2.size(); j++) {
+                int sum = nums1[i]  + nums2[j];
                 
+                /*this doesn't use the fact that these two arrays are sorted
+                maxHeap.push({nums1[i] + nums2[j], {nums1[i], nums2[j]}});
+                if(maxHeap.size() > k) maxHeap.pop();
+                I'm just making all the pairs in brute-force way. I can optimize this.*/\
+                   
+                //if k element pairs are not filled, add this pair.    
                 if(maxHeap.size() < k) maxHeap.push({sum, {nums1[i], nums2[j]}});
-                else if(sum < maxHeap.top().first) {
-                    maxHeap.pop();
-                    maxHeap.push({sum, {nums1[i], nums2[j]}});
-                } else {
-                    break;
-                }
+                //if k or more elements are filled, check whether new element pair
+                //has a sum less than the top most element pair.
+                //if yes, pop top element pair and add this instead.
+                else if(sum < maxHeap.top().second.first + maxHeap.top().second.second) {
+                    maxHeap.pop(); 
+                    maxHeap.push({sum, {nums1[i],  nums2[j]}});
+                } else break;
+            
             }
         }
         
         vector<vector<int>> ans(maxHeap.size());
         int i = 0;
         while(!maxHeap.empty()) {
-            int x = maxHeap.top().second.first;
-            int y = maxHeap.top().second.second;
-            ans[i].push_back(x);
-            ans[i].push_back(y);
+            ans[i].push_back(maxHeap.top().second.first);
+            ans[i].push_back(maxHeap.top().second.second);
             maxHeap.pop();
             i++;
         }
+        
         reverse(ans.begin(), ans.end());
         return ans;
+        
     }
 };
