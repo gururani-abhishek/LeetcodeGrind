7393c2b2f475629639339e881c502d583cab4f6a
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-18 11:49:22 +0530

    Time: 40 ms (68.61%), Space: 14.3 MB (37.24%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int l = matrix[0][0], r = matrix[matrix.size()-1][matrix[0].size() -1];
-        while(l < r) {
-            int mid = l + (r -l)/2;
-            
-            if(countElements(matrix, mid) >= k) r = mid; 
-            else l = mid + 1;
+        priority_queue<int> maxHeap;
+        int n = matrix.size();
+        
+        for(int i=0; i<n; i++) { 
+            for(int j=0; j<n; j++) {
+                maxHeap.push(matrix[i][j]);
+                
+                if(maxHeap.size() > k) maxHeap.pop();
+            }
         }
-    
-        return l;
-    }
-private: 
-    int countElements(vector<vector<int>> matrix, int k) {
-        int count = 0;
-        int j = matrix[0].size() -1;
-        for(int i=0; i<matrix.size(); i++) {
-            while(j >= 0 && matrix[i][j] > k) j--;
-            count += j + 1;
-        }
-        return count;
+        
+        return maxHeap.top();
+        
     }
 };
+
+//tc: O(n*n*log(k)) //building a heap of size 'k'
+//sc: O(k) //heap of size k
