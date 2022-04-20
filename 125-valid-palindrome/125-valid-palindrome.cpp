f9ba83328d7cc9fa496dfc3acf403cae8a0a3a6f
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-20 09:35:32 +0530

    Time: 4 ms (87.02%), Space: 7.4 MB (50.87%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -5,17 +5,32 @@ public:
         //keep letters and numbers only
         //tc = O(n), sc = O(n);
         int n = s.size();
-        string s1;
-        for(int i=0; i<n; i++) {
-            if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' || s[i] >='0' && s[i] <= '9') {
-                if(s[i] >= 'A' && s[i] <= 'Z') s1 += s[i] + 32;
-                else s1 += s[i];
-            }    
-        }
-        int i =0, j = s1.length()-1;
+    
+        int i =0, j = s.length()-1;
+        
+        bool fi = 0; 
+        bool fj = 0;
         while(i < j) {
-            if(s1[i] != s1[j]) return false;
-            i++, j--;
+            if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' || s[i] >='0' && s[i] <= '9') {
+                if(s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
+                fi = 1;
+            } 
+            
+            if(s[j] >= 'a' && s[j] <= 'z' || s[j] >= 'A' && s[j] <= 'Z' || s[j] >= '0' && s[j] <= '9') {
+                if(s[j] >= 'A' && s[j] <= 'Z') s[j] += 32;
+                fj = 1;
+            }
+            
+            if(fi && fj) {
+                if(s[i] != s[j]) return false;
+                i++, j--;
+            } else if(!fi && !fj) i++, j--;
+            else if(!fi) i++;
+            else if(!fj) j--;
+            
+            
+            fi = 0;
+            fj = 0;
         }
         
         return true;
