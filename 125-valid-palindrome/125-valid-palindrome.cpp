5bc3445b26db084dcb5ad05efc14e8e4c012b503
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-20 09:45:47 +0530

    Time: 4 ms (87.02%), Space: 7.4 MB (75.96%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        //uppercase to lowercase
-        //keep letters and numbers only
-        //tc = O(n), sc = O(n);
+        //uppercase to lowercase: tolower() 
+        //keep letters and numbers only: isalnum() checks if the number is alphabet numeric or not.
+        //tc = O(n), sc = O(1);
         int n = s.size();
     
         int i =0, j = s.length()-1;
         
-        bool fi = 0; 
-        bool fj = 0;
         while(i < j) {
-            if(s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z' || s[i] >='0' && s[i] <= '9') {
-                if(s[i] >= 'A' && s[i] <= 'Z') s[i] += 32;
-                fi = 1;
-            } 
-            
-            if(s[j] >= 'a' && s[j] <= 'z' || s[j] >= 'A' && s[j] <= 'Z' || s[j] >= '0' && s[j] <= '9') {
-                if(s[j] >= 'A' && s[j] <= 'Z') s[j] += 32;
-                fj = 1;
+            while(i < j && !isalnum(s[i])) {
+                i++;
+            }
+            while(j > i && !isalnum(s[j])) {
+                j--;
             }
             
-            if(fi && fj) {
-                if(s[i] != s[j]) return false;
-                i++, j--;
-            } else if(!fi && !fj) i++, j--;
-            else if(!fi) i++;
-            else if(!fj) j--;
-            
-            
-            fi = 0;
-            fj = 0;
+            if(tolower(s[i]) != tolower(s[j])) return false;
+            i++, j--;
         }
         
         return true;
