dab8a64a740496934b9725a37abf331a3cc05623
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-04-24 16:14:25 +0530

    Time: 7 ms (74.05%), Space: 12.1 MB (17.05%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     vector<string> ans;
     vector<string> letterCasePermutation(string s) {
-        string op = "";
-        create(op, s);
-        
+        string s1 = "";
+        create(s1, s);
         return ans;
     }
-private:
     
-    void create(string op, string ip) {
-        if(ip.length() == 0) {
-            ans.push_back(op);
-            return;
+private:
+    void create(string s1, string s)  {
+        if(s.size() == 0) {
+           ans.push_back(s1);
+           return;
         }
-    //isalpha(): returns 0 if not an alphabet, else non zero
-    //tolower(): to lowercase
-    //toupper(): to uppercase
-
-        string op1 = op;
-        char x = ip[0];
         
-        op.push_back(ip[0]);
-        ip.erase(ip.begin() + 0);
-        create(op, ip);
+        char x= s[0];
+        s.erase(s.begin() + 0);
         
         if(isalpha(x)) {
-            x ^= (1<<5); //toggles, lower to upper and upper to lower.
-            op1.push_back(x);
-            create(op1, ip);
-        }
+            string s2 = s1;
+            s1.push_back(toupper(x));
+            s2.push_back(tolower(x));
+            create(s1, s);
+            create(s2, s);
         
+        }else {
+            s1.push_back(x);
+            create(s1, s);    
+        }
     }
 };
