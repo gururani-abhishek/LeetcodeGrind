506a729db427d69292174d6b84b01311bf506661
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-05-01 23:18:52 +0530

    Time: 648 ms (11.78%), Space: 165 MB (9.04%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> ans1;
+        int nums = n;
+        create(nums, 1, k, ans1);
+        return ans;
+    }
+
+private:
+    void create(int nums, int i, int k, vector<int> ans1) {
+       if(!k) {
+           ans.push_back(ans1);
+           return;
+        }
+        
+        for(int j = i; j<=nums; j++) {
+            ans1.push_back(j);
+            create(nums, j + 1, k-1, ans1);
+            ans1.pop_back();
+        }
+        
+    }
+};
