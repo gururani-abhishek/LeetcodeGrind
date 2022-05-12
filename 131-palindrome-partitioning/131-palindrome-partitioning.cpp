7323ce8175064de627fb59cba1a0a3e687b28e25
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-05-12 15:50:22 +0530

    Time: 173 ms (51.54%), Space: 79.9 MB (34.95%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<vector<string>> partition(string s) {
+        vector<string> ans1;
+        create(s, ans1, 0);
+        return ans;
+    }
+    
+private:
+    void create(string s, vector<string>& ans1, int point) {
+        if(point == s.size()) {
+            ans.push_back(ans1);
+            return;
+        }
+        
+        string str = "";
+        for(int i =point; i<s.size(); i++) {
+            str.push_back(s[i]);
+            int start =0, end = str.size() -1, flag = 0;
+            while(start< end) {
+                if(str[start] != str[end]) {flag = 1; break;}
+                start++, end--;
+            }
+            if(flag) continue;
+            
+            ans1.push_back(str);
+            create(s, ans1, i+ 1);
+            ans1.pop_back();
+        }
+        
+    }
+};
