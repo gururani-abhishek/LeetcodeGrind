b629d7393ef2ad998bde68c4b5d45e6e9ca0bc84
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-05-27 15:03:17 +0530

    Time: 0 ms (100.00%), Space: 13 MB (46.93%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,74 +9,43 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, TreeNode*> cp;
-//     void inorder(TreeNode* root) {
-//         //if(root == NULL) return;
-        
-//         if(root -> left != NULL) {
-//             cp[root -> left] = root;
-//             inorder(root -> left);
-//         }
-        
-//         if(root -> right != NULL) {
-//             cp[root -> right] = root;
-//             inorder(root ->  right);
-//         }
-//     }
-    
-    void inorder(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        
-            while(!q.empty()) {
-                int size = q.size();
-                
-                while(size--) {
-                    TreeNode* node = q.front();
-                    q.pop();
-                    
-                    if(node -> left != NULL) {
-                        cp[node -> left] = node;
-                        q.push(node -> left);
-                    }
-                    
-                    if(node -> right != NULL) {
-                        cp[node -> right] = node;
-                        q.push(node -> right);
-                    }
-                }
-            }
-        
-    }
+    unordered_map<TreeNode*, TreeNode*> parent; //maps child to parent
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         unordered_map<TreeNode*, bool> visited;
-        inorder(root);
+        dfs(root);
         queue<TreeNode*> q;
         q.push(target);
-        int dist = 0;
         
+        int dist = 0;
         visited[target] = true;
+        
         while(!q.empty()) {
             int size = q.size();
+            
             if(dist++ == k) break;
             
             while(size--) {
                 TreeNode* node = q.front();
                 q.pop();
                 
+                //radially outward movement to: 
+                
+                //left child
                 if(!visited[node -> left] && node -> left != NULL) {
-                    q.push(node -> left);
-                    visited[node -> left] = true;   
+                   visited[node -> left] = true;
+                   q.push(node -> left); 
                 }
                 
+                //right child
                 if(!visited[node -> right] && node -> right != NULL) {
-                    q.push(node -> right);
                     visited[node -> right] = true;
+                    q.push(node -> right);
                 }
                 
-                if(cp[node]  && !visited[cp[node]]) {
-                    q.push(cp[node]);
-                    visited[cp[node]] = true;
+                //parent
+                if(parent[node] && !visited[parent[node]]) {
+                    visited[parent[node]] = true;
+                    q.push(parent[node]);
                 }
             }
         }
@@ -85,7 +54,27 @@ public:
         while(!q.empty()) {
             ans.push_back(q.front() -> val);
             q.pop();
-        }
+        }        
+        
         return ans;
     }
+
+private:
+    void dfs(TreeNode* root) {
+        if(root == NULL) return;
+        
+        if(root -> left != NULL) {
+            parent[root -> left] = root;
+            dfs(root -> left);
+        }
+        
+        if(root -> right != NULL) {
+            parent[root -> right] = root;
+            dfs(root -> right);
+        }
+    }
 };
+
+
+//idea is to move radially outward from our target node, 
+//radially outward means, from node : to parent node, to left child, to right child.
