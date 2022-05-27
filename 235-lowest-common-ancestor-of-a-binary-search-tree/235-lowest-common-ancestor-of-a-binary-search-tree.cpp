32b5e7f57c07731ab641351e894e55412bf59617
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-05-27 23:04:51 +0530

    Time: 45 ms (31.19%), Space: 23.3 MB (13.48%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,9 +11,11 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root -> val > p -> val && root -> val > q -> val)    return lowestCommonAncestor(root -> left, p, q); 
-        else if(root -> val < p -> val && root -> val < q -> val)   return lowestCommonAncestor(root -> right, p, q);
-        
+        if(root -> val < p -> val && root -> val < q -> val) return lowestCommonAncestor(root -> right, p, q);
+        else if(root -> val > p -> val && root -> val > q -> val) return lowestCommonAncestor(root -> left, p, q);
+    
         return root;
     }
 };
+
+//take {p,q} as a package, traverse to them using the properties of BST, and the moment they divide, that's our answer.
