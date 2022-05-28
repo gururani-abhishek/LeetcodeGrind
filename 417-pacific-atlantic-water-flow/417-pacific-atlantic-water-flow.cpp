8408d7b51a34fce0d74b4e3157c944c7fab560f7
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-05-28 13:56:26 +0530

    Time: 75 ms (35.37%), Space: 25.8 MB (17.70%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    
     vector<vector<int>> pacific, atlantic;
     vector<vector<int>> ans;
-    
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int n = heights.size(), m = heights[0].size();
         pacific = atlantic = vector<vector<int>> (n, vector<int> (m, 0));
+    
+        for(int i=0; i<n; i++) dfs(heights, pacific, i, 0), dfs(heights, atlantic, i, m-1);
+        for(int j = 0; j<m; j++) dfs(heights, pacific, 0, j), dfs(heights, atlantic, n-1, j);
         
-        for(int i = 0; i<n; i++) explore(heights, i, 0, pacific), explore(heights, i, m-1, atlantic);
-        for(int j = 0; j<m; j++) explore(heights, 0, j, pacific), explore(heights, n-1, j, atlantic);
-    
-    
         return ans;
     }
     
 private: 
-    void explore(vector<vector<int>>& heights, int i, int j, vector<vector<int>>& visited) {
+    void dfs(vector<vector<int>>& heights, vector<vector<int>>& visited, int i, int j) {
         if(visited[i][j]) return;
         visited[i][j] = 1;
         
-        if(pacific[i][j] && atlantic[i][j]) ans.push_back(vector<int> {i, j});
+        if(atlantic[i][j] && pacific[i][j]) ans.push_back(vector<int>{i, j});
         
-        if(i+1 < heights.size() && heights[i][j] <= heights[i+1][j]) explore(heights, i+1, j, visited);
-        if(i-1 >= 0 && heights[i][j] <= heights[i-1][j]) explore(heights, i-1, j, visited);
-        if(j+1 < heights[0].size() && heights[i][j] <= heights[i][j+1]) explore(heights, i, j+1, visited);
-        if(j-1 >= 0 && heights[i][j] <= heights[i][j-1]) explore(heights, i, j-1, visited);
+        if(i-1 >= 0 && heights[i][j] <= heights[i-1][j]) dfs(heights, visited, i-1, j);
+        if(i+1 < heights.size() && heights[i][j] <= heights[i+1][j]) dfs(heights, visited, i+1, j);
+        if(j+1 < heights[0].size() && heights[i][j] <= heights[i][j+1]) dfs(heights, visited, i, j+1);
+        if(j-1 >= 0 && heights[i][j] <= heights[i][j-1]) dfs(heights, visited, i, j-1);
+        
     }
 };
+
+//          {i-1, j}
+// {i, j-1} {i, j} {i, j+1}
+//          {i+1, j}
