ab72a4a31001a7ae758aaba887ab395ac09f9708
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-05-29 13:47:51 +0530

    Time: 15 ms (20.31%), Space: 13 MB (46.59%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,44 +9,34 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, TreeNode*> parent; //maps child to parent
+    unordered_map<TreeNode*, TreeNode*> parent;
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*, bool> visited;
         dfs(root);
+        unordered_map<TreeNode*, bool> visited;
         queue<TreeNode*> q;
         q.push(target);
         
         int dist = 0;
-        visited[target] = true;
         
+        visited[target] = true;
         while(!q.empty()) {
-            int size = q.size();
-            
             if(dist++ == k) break;
-            
+            int size = q.size();
             while(size--) {
-                TreeNode* node = q.front();
-                q.pop();
-                
-                //radially outward movement to: 
-                
-                //left child
-                if(!visited[node -> left] && node -> left != NULL) {
-                   visited[node -> left] = true;
-                   q.push(node -> left); 
-                }
-                
-                //right child
-                if(!visited[node -> right] && node -> right != NULL) {
-                    visited[node -> right] = true;
-                    q.push(node -> right);
-                }
-                
-                //parent
-                if(parent[node] && !visited[parent[node]]) {
-                    visited[parent[node]] = true;
-                    q.push(parent[node]);
-                }
+            TreeNode* node = q.front();
+            q.pop();
+            visited[node] = true;
+            if(node -> left != NULL && !visited[node -> left]) {
+                q.push(node -> left);
+            }
+            
+            if(node -> right != NULL && !visited[node -> right]) {
+                q.push(node -> right);
+            }
+            
+            if(parent[node] && !visited[parent[node]]) {
+                q.push(parent[node]);
+            }
             }
         }
         
@@ -54,12 +44,12 @@ public:
         while(!q.empty()) {
             ans.push_back(q.front() -> val);
             q.pop();
-        }        
+        }
         
         return ans;
     }
 
-private:
+private: 
     void dfs(TreeNode* root) {
         if(root == NULL) return;
         
@@ -75,6 +65,3 @@ private:
     }
 };
 
-
-//idea is to move radially outward from our target node, 
-//radially outward means, from node : to parent node, to left child, to right child.
