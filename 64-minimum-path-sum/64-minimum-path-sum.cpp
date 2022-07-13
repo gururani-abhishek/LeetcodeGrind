4964c0d1cbc74966b13662681265339957aff585
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-13 18:59:24 +0530

    Time: 23 ms (15.22%), Space: 10.1 MB (28.77%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -3,15 +3,26 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        return minpathsum(grid, n-1, m-1, dp);
-    }
-private: 
-    int minpathsum(vector<vector<int>>& grid, int row, int col, vector<vector<int>>& dp) {
-        if(row == 0 && col == 0) return grid[row][col];
-        else if(row < 0 || col < 0) return INT_MAX;
+
+        dp[0][0] = grid[0][0];
         
-        if(dp[row][col] != -1) return dp[row][col];
-        return dp[row][col] = grid[row][col] + min(minpathsum(grid, row-1, col, dp), minpathsum(grid,  row, col-1, dp));
+        for(int row=0; row<n; row++) {
+            for(int col=0; col<m; col++) {
+                if(row == 0 && col == 0) continue;
+                else {
+                    int left = 0, up = 0;
+//                     if(row > 0) up = dp[row-1][col];
+//                     else up = INT_MAX;
+                     
+                    up = row > 0 ? dp[row-1][col] : INT_MAX;
+                    left = col > 0 ? dp[row][col -1] : INT_MAX;
+                    
+                    dp[row][col] = grid[row][col] + min(left, up);
+                }
+            }
+        }
         
+        return dp[n-1][m-1];
     }
+
 };
