8e1bce71d5ae12da51ef3d9520efd4c052ccb2a8
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-14 20:56:31 +0530

    Time: 8 ms (67.06%), Space: 9 MB (22.53%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size(), -1));
+        return minTB(triangle, 0, 0, dp);
+    }
+    
+    
+private: 
+    int minTB(vector<vector<int>>& triangle, int row, int idx, vector<vector<int>>& dp) {
+        if(row == triangle.size() -1) return triangle[row][idx];
+        else if(idx > triangle[row].size()-1) return INT_MAX;
+        
+        
+        if(dp[row][idx] != -1) return dp[row][idx];
+        
+        int down = triangle[row][idx], downFront = triangle[row][idx];
+        down += minTB(triangle, row+1,idx, dp);
+        downFront += minTB(triangle, row+1, idx+1, dp);
+        
+        return dp[row][idx] = min(down, downFront);
+    
+    
+    }
+};
