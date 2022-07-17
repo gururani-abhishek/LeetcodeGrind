dcbeb3523f9787b56e2ea438d7e08b2c9682c4fd
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-17 11:29:36 +0530

    Time: 271 ms (14.96%), Space: 17 MB (9.17%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        vector<vector<vector<int>>> dp(grid.size(), vector<vector<int>>(grid[0].size(), vector<int>(grid[0].size(), -1)));
-        return maxCherryPicked(grid, 0, 0, grid[0].size() -1, dp);
-    }
-    
-    int maxCherryPicked(vector<vector<int>>& grid, int i, int j1, int j2, vector<vector<vector<int>>>& dp) {
-        if(j1 < 0 || j2 < 0 || j1 > grid[0].size()-1 || j2 > grid[0].size() -1) {
-            return -1e9;
-        }
+        int n = grid.size(), m = grid[0].size();
         
-        if(i == grid.size()-1) {
-            if(j1 == j2) return grid[i][j1];
-            else return (grid[i][j1] + grid[i][j2]);
-        }
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0))); //3d vector dp
         
-        
-        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-        
-        int mx = -1e9;
-        for(int dj1 = -1; dj1 <2; dj1++) {
-            for(int dj2 = -1; dj2 <2; dj2++) {
-                if(j1 != j2) mx = max(mx, grid[i][j1] + grid[i][j2] + maxCherryPicked(grid, i+1, j1+ dj1, j2+ dj2, dp));
-                else mx = max(mx, grid[i][j1] + maxCherryPicked(grid, i+1, j1+dj1, j2+dj2, dp));
+        for(int j1 = 0; j1<m; j1++) {
+            for(int j2 = 0; j2<m; j2++) {
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1]; 
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
             }
         }
         
-        return dp[i][j1][j2] = mx;
+        for(int i=n-2; i>=0; i--) {
+            for(int j1 = 0; j1 < m; j1++) {
+                for(int j2 = 0; j2 < m; j2++) {
+                    
+                    int mx = -1e9;
+                    for(int dj1 = -1; dj1 <2; dj1++) {
+                        for(int dj2 = -1; dj2 <2; dj2++) {
+                            
+                            int mov = (j1+dj1 >= 0 && j1+dj1 <= m-1 && j2+dj2 >=0 && j2+dj2 <= m-1) ? dp[i+1][dj1+j1][dj2+j2] : -1e9; 
+                            if(j1 == j2) mov += grid[i][j1];
+                            else mov += grid[i][j1] + grid[i][j2];
+                            
+                            mx = max(mov, mx);
+                        }
+                    }
+                    
+                    
+                    dp[i][j1][j2] = mx;
+                }
+            }
+        }
+        
+        
+         return dp[0][0][m-1];   
+        
+        
     }
 };
