02b365e2b6f7117b7c3a1e72c05179fa18c2671f
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-17 11:33:52 +0530

    Time: 273 ms (14.66%), Space: 9.3 MB (92.37%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,12 +3,16 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0))); //3d vector dp
+        //space optimisation as only prev row is being used I can simply take 2 2d arrays, to store curr and prev data. 
+        //tc : O(n*m*m*9)
+        //sc : O(m*m)
+        vector<vector<int>> prev(m, vector<int>(m, 0));
+        vector<vector<int>> curr(m, vector<int>(m, 0));
         
         for(int j1 = 0; j1<m; j1++) {
             for(int j2 = 0; j2<m; j2++) {
-                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1]; 
-                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+                if(j1 == j2) prev[j1][j2] = grid[n-1][j1]; 
+                else prev[j1][j2] = grid[n-1][j1] + grid[n-1][j2];
             }
         }
         
@@ -20,7 +24,7 @@ public:
                     for(int dj1 = -1; dj1 <2; dj1++) {
                         for(int dj2 = -1; dj2 <2; dj2++) {
                             
-                            int mov = (j1+dj1 >= 0 && j1+dj1 <= m-1 && j2+dj2 >=0 && j2+dj2 <= m-1) ? dp[i+1][dj1+j1][dj2+j2] : -1e9; 
+                            int mov = (j1+dj1 >= 0 && j1+dj1 <= m-1 && j2+dj2 >=0 && j2+dj2 <= m-1) ? prev[dj1+j1][dj2+j2] : -1e9; 
                             if(j1 == j2) mov += grid[i][j1];
                             else mov += grid[i][j1] + grid[i][j2];
                             
@@ -29,13 +33,14 @@ public:
                     }
                     
                     
-                    dp[i][j1][j2] = mx;
+                    curr[j1][j2] = mx;
                 }
             }
+            prev = curr;
         }
         
         
-         return dp[0][0][m-1];   
+         return prev[0][m-1];   
         
         
     }
