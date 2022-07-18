b8a30b162f9bd57abde2806a28d7f3dd2f870319
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-18 14:06:16 +0530

    Time: 282 ms (54.62%), Space: 9.3 MB (89.18%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -6,26 +6,28 @@ public:
         
         if(sum %2) return false;
         //now this problem is : "subset sum equals to sum/2" problem
-        vector<vector<bool>> dp(n, vector<bool>(sum/2+1, 0));
+        vector<bool> prev(sum/2 + 1, false);
+        vector<bool> curr(sum/2 + 1, false);
         
         //define basecases: 
         //1st bC: doesn't matter what idx is, if target == 0, return true;
-        for(int idx=0; idx<n; idx++) dp[idx][0] = true;
+        for(int idx=0; idx<n; idx++) prev[0] = true;
         
         //2nd bC: when idx is equal to 0 and target == nums[0], return true;
-        if(nums[0] <= sum/2 + 1) dp[0][nums[0]] = true;
+        if(nums[0] <= sum/2 + 1) prev[nums[0]] = true;
         
         for(int idx=1; idx<n; idx++) {
             for(int target = 1; target <= sum/2; target++) {
             bool leaveIt = false, takeIt = false;
-            leaveIt = dp[idx-1][target]; 
+            leaveIt = prev[target]; 
         
-            if(nums[idx] <= target) takeIt = dp[idx-1][target-nums[idx]];
+            if(nums[idx] <= target) takeIt = prev[target-nums[idx]];
         
-            dp[idx][target] = (takeIt  || leaveIt);
+            curr[target] = (takeIt  || leaveIt);
             }
+            prev = curr;
         }
         
-        return dp[n-1][sum/2];
+        return prev[sum/2];
     }
 };
