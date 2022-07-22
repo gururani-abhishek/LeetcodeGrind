df294bb19d7a4a6be25ee9af0d669a0b47fb56b2
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-22 16:26:46 +0530

    Time: 58 ms (16.70%), Space: 13.2 MB (21.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return create(text1, text2, n, m, dp);
-    }
-    
-private: 
-    int create(string&s1, string&s2, int idx1, int idx2, vector<vector<int>>& dp) {
-        if(idx1 == 0 || idx2 == 0) return 0;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        if(s1[idx1 -1] == s2[idx2-1]) return 1 + create(s1, s2, idx1-1, idx2-1, dp);
+        //basecases: 
+        for(int idx2 = 0; idx2 <= m; idx2++) dp[0][idx2] = 0;
+        for(int idx1 = 0; idx1 <= n; idx1++) dp[idx1][0] = 0;
         
-        return dp[idx1][idx2] = max(create(s1, s2, idx1-1, idx2, dp), create(s1, s2, idx1, idx2-1, dp));
+        for(int idx1 = 1; idx1 <= n; idx1++) {
+            for(int idx2 = 1; idx2 <=m; idx2++) {
+                if(text1[idx1 -1] == text2[idx2-1]) {
+                    dp[idx1][idx2] = 1 + dp[idx1-1][idx2-1];
+                    continue;
+                }
+                
+                dp[idx1][idx2] = max(dp[idx1-1][idx2], dp[idx1][idx2-1]);        
+            }
+        }
+        
+        return dp[n][m];        
     }
 };
