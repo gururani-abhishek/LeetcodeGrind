f7690326dcc05618df1a1ecca880a3fb9bb7fb2a
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-23 21:09:08 +0530

    Time: 903 ms (13.14%), Space: 292.3 MB (6.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return create(0, -1, nums, dp);
-    }
-    
-private: 
-    int create(int idx, int prev_idx, vector<int>& nums, vector<vector<int>>& dp) {
-       
-        if(idx == nums.size() -1) {
-            if(prev_idx == -1 || nums[idx] > nums[prev_idx]) return 1;
-            return 0;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        for(int idx=n-1; idx>=0; idx--) {
+            for(int prev_idx = idx-1; prev_idx >= -1; prev_idx--) {
+                
+            int take = 0, nottake = dp[idx+1][prev_idx+1];
+            if(prev_idx == -1 || nums[idx] > nums[prev_idx]) take = 1 + dp[idx +1][idx+1];
+
+            dp[idx][prev_idx+1] = max(take, nottake);
+            
+            }
         }
         
-        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx + 1];
-        int take = 0, nottake = create(idx+1, prev_idx, nums, dp);
-        if(prev_idx == -1 || nums[idx] > nums[prev_idx]) take = 1 + create(idx +1, idx, nums, dp);
-        
-        return dp[idx][prev_idx+1] = max(take, nottake);
-        
+        return dp[0][-1+1]; //+1 to prev_idx for coordinate adjustment.
     }
+    
 };
