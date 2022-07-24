c19a12140e464ab2412460274c8df2379420f210
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-25 00:08:48 +0530

    Time: 6 ms (19.46%), Space: 8.2 MB (6.53%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -5,27 +5,25 @@ public:
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
         
-        vector<int> dp(n, -1);
-        int start_with_zeroIdx = create(0, nums, n-2, dp);
-        
-        vector<int> dp1(n, -1);
-        int start_with_oneIdx = create(1, nums, n-1, dp1);
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        int start_with_zeroIdx = create(0, nums, n-2, dp, 0);
+        int start_with_oneIdx = create(1, nums, n-1, dp, 1);
         
         return max(start_with_oneIdx, start_with_zeroIdx);
     }
 
 private: 
-    int create(int idx, vector<int>& nums, int size, vector<int>& dp) {
+    int create(int idx, vector<int>& nums, int size, vector<vector<int>>& dp, int forDp) {
         if(idx == size) {
             return nums[idx];
         }
         
-        if(dp[idx] != -1) return dp[idx];
-        int npick = create(idx +1, nums, size, dp);
-        int pick = nums[idx] + ((idx + 2 <= size) ? create(idx + 2, nums, size, dp) : 0);
+        if(dp[idx][forDp] != -1) return dp[idx][forDp];
+        int npick = create(idx +1, nums, size, dp, forDp);
+        int pick = nums[idx] + ((idx + 2 <= size) ? create(idx + 2, nums, size, dp, forDp) : 0);
         
         
-        return dp[idx] = max(pick, npick);
+        return dp[idx][forDp] = max(pick, npick);
     }
 
 };
