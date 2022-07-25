06f65e19dd364d78b49f071b35aa203615542cf3
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-25 19:19:42 +0530

    Time: 9 ms (51.35%), Space: 7.9 MB (46.78%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
+        //if you'll go by logic the only way to solve this question was through O(nlogn);
+        //but there's an underlying pattern, ans[0] = 0, ans[1] = 1;
+        //then, if odd then -> (no. of ones = no. of ones in it's half + 1);
+        //if even then -> (no. of ones = no. of ones in it's half);
         
-        for(int i=0; i<=n; i++) 
-            ans.push_back(count(i));
-    
-        return ans;
-    }
-
-int count(int num) {
-        int res = 0;
-        while(num > 0) {
-            res += (num %2);
-            num /= 2;
+        //tc : o(n), sc : o(n);
+        
+        //edge cases: 
+        if(n == 0) return {0};
+        if(n == 1) return {0, 1};
+        vector<int> ans(n+1); 
+        //ans[0] = 0, ans[1] = 1;
+        ans[0] = 0;
+        ans[1] = 1;
+        
+        for(int num = 2; num <= n; num++) {
+            if(num % 2) ans[num] = ans[num/2] + 1; //odd num
+            else ans[num] = ans[num/2]; //even num
         }
-
-    return res;
+        
+        return ans;
     }
 };
