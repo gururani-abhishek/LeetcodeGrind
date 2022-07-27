e0778ffb73fd36ac5fec493eef012a9677887856
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-27 18:41:23 +0530

    Time: 175 ms (48.90%), Space: 73.2 MB (7.91%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -3,26 +3,19 @@ public:
     int longestPalindromeSubseq(string s) {
         string revs = s;
         reverse(revs.begin(), revs.end());
-        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1, 0));
-        
-        for(int idxs=0; idxs<=s.size(); idxs++) dp[idxs][s.size()] = 0;
-        for(int idxrevs=0; idxrevs<=s.size(); idxrevs++) dp[s.size()][idxrevs] = 0;
-        
-        for(int idxs = s.size()-1; idxs >= 0; idxs--) {
-            for(int idxrevs = s.size()-1; idxrevs >= 0; idxrevs--) {
-                if(s[idxs] == revs[idxrevs]) {
-                    dp[idxs][idxrevs] = 1 + dp[idxs+1][idxrevs+1];
-                    continue;        
-                }
-                
-            //they do not match;
-            dp[idxs][idxrevs] = max(dp[idxs+1][idxrevs], dp[idxs][idxrevs + 1]);
-
-            }
-        }
-        
-        return dp[0][0];
-        
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
+        return create(s, revs, 0, 0, dp); 
     }
 
+private: 
+    int create(string &s, string &revs, int idxs, int idxrevs, vector<vector<int>>& dp) {
+        if(idxs >= s.length() || idxrevs >= revs.length()) return 0;
+        
+        if(dp[idxs][idxrevs] != -1) return dp[idxs][idxrevs];
+        //they match 
+        if(s[idxs] == revs[idxrevs]) return dp[idxs][idxrevs] = 1 + create(s, revs, idxs+1, idxrevs+1, dp);
+        
+        //they do not match;
+        return dp[idxs][idxrevs] = max(create(s, revs, idxs+1, idxrevs, dp), create(s, revs, idxs, idxrevs + 1, dp));
+    }
 };
