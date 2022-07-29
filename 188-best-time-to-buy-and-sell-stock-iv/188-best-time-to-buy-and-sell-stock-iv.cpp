8ac2da3ab76416067d3099b57c2d15ae830cba80
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-29 23:54:31 +0530

    Time: 21 ms (44.36%), Space: 12.7 MB (41.58%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,29 +2,36 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2*k, -1));
-        return create(prices, 0, 0, k, dp);
-    }
-    
-private:
-    
-    int create(vector<int>& prices, int idx, int bought, int limit, vector<vector<int>>& dp) {
-        if(idx == prices.size()) return 0;
-        if(bought == 2*limit) return 0;
-
-        if(dp[idx][bought] != -1) return dp[idx][bought];
-        //bought = 0, 2, 4... where I can buy the stocks.
-        if(bought % 2 == 0) {
-            int buy = -1* prices[idx] + create(prices, idx + 1, bought+1, limit, dp);
-            int nbuy = create(prices, idx + 1, bought, limit, dp);
-        
-            return dp[idx][bought] = max(buy, nbuy);
+        vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
+        for(int bought = 0; bought <= 2*k; bought++) {
+            dp[n][bought] = 0;
         }
         
-        //bought = 1, 3, 5...where  I can sell the stocks.
-        int sell = prices[idx] + create(prices, idx + 1, bought + 1, limit, dp);
-        int nsell = create(prices, idx + 1, bought, limit, dp);
+        for(int idx = 0; idx <= n; idx++) {
+            dp[idx][2*k] = 0;
+        }
         
-        return dp[idx][bought] = max(sell, nsell);
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int bought = 2*k-1; bought >= 0; bought--) {
+                 if(bought % 2 == 0) {
+                    int buy = -1* prices[idx] + dp[idx + 1][bought+1];
+                    int nbuy = dp[idx + 1][bought];
+
+                    dp[idx][bought] = max(buy, nbuy);
+                    continue;
+                 }
+        
+        //bought = 1, 3, 5...where  I can sell the stocks.
+                int sell = prices[idx] + dp[idx + 1][bought + 1];
+                int nsell = dp[idx + 1][bought];
+
+                dp[idx][bought] = max(sell, nsell);
+            }
+        }
+        
+        
+        
+        return dp[0][0];
     }
+    
 };
