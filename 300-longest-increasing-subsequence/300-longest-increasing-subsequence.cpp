aa5b20b9328777f1f71c8e00e52cc35f370ebbb3
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-30 17:13:23 +0530

    Time: 2126 ms (5.01%), Space: 292.3 MB (8.65%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,26 +4,28 @@ public:
         //instead of passing huge prev number, using which it'll be hard to memoize the solution
         //i'm passing, the idx of previous number.
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return create(nums, 0, -1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        //during memoization I did coordinate shifting, so
+        //every index for prev_idx will be represented by a plus 1
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int prev_idx = -1; prev_idx < n; prev_idx++) {
+                
+                if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
+                    int takeIt = 1 + dp[idx +1][idx+1];
+                    int notPickIt = dp[idx +1][prev_idx+1];
+
+                    dp[idx][prev_idx + 1] = max(takeIt, notPickIt);
+                    continue;
+                }
+
+
+                dp[idx][prev_idx + 1] = dp[idx +1][prev_idx+1];                
+                
+            }
+        }
+        
+        return dp[0][-1 + 1];
     }
     
-private: 
-    int create(vector<int>& nums, int idx, int prev_idx, vector<vector<int>>& dp) {
-        if(idx == nums.size()) {
-            return 0;
-        }
-        
-        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx + 1];
-        
-        if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
-            int takeIt = 1 + create(nums, idx +1, idx, dp);
-            int notPickIt = create(nums, idx +1, prev_idx, dp);
-            
-            return dp[idx][prev_idx + 1] = max(takeIt, notPickIt);
-        }
-        
-
-        return dp[idx][prev_idx + 1] = create(nums, idx +1, prev_idx, dp);
-    }
 };
