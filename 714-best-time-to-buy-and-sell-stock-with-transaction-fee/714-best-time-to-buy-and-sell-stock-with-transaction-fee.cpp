2695b7d2abdb61796591d49619a7bb057cff6ecd
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-30 15:58:23 +0530

    Time: 393 ms (18.58%), Space: 91 MB (33.30%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return create(prices, fee, 0, 0, dp);
-    }
-    
-private: 
-    int create(vector<int>& prices, int fee, int idx, int bought, vector<vector<int>>& dp) {
-        if(idx == prices.size()) return 0;
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        dp[n][0] = dp[n][1] = 0;
         
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int bought = 0; bought <2; bought++) {
+                if(bought) {
+                    int sell = prices[idx] - fee + dp[idx + 1][!bought];
+                    int nsell = dp[idx + 1][bought];
+
+                    dp[idx][bought] = max(sell, nsell);
+                    continue;
+                }
         
-        if(dp[idx][bought] != -1) return dp[idx][bought];
-        if(bought) {
-            int sell = prices[idx] - fee + create(prices, fee, idx + 1, !bought, dp);
-            int nsell = create(prices, fee, idx + 1, bought, dp);
-        
-            return dp[idx][bought] = max(sell, nsell);
+                int buy = -prices[idx] + dp[idx+1][!bought];
+                int nbuy = dp[idx + 1][bought];
+
+                dp[idx][bought] = max(buy, nbuy);
+            }
         }
         
-        int buy = -prices[idx] + create(prices,  fee, idx+1, !bought, dp);
-        int nbuy = create(prices, fee, idx + 1, bought, dp);
-        
-        return dp[idx][bought] = max(buy, nbuy);
-        
+       return dp[0][0];
     }
+    
 };
