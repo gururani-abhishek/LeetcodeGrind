e1e56d61e5aaf6211efe13f120cf4d2aa21f4dd5
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-30 16:49:58 +0530

    Time: 2921 ms (5.01%), Space: 292.2 MB (8.65%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,28 +4,26 @@ public:
         //instead of passing huge prev number, using which it'll be hard to memoize the solution
         //i'm passing, the idx of previous number.
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
         return create(nums, 0, -1, dp);
     }
     
 private: 
     int create(vector<int>& nums, int idx, int prev_idx, vector<vector<int>>& dp) {
-        if(idx == nums.size()-1) {
-            if(prev_idx == -1 || nums[idx] > nums[prev_idx]) return 1;
+        if(idx == nums.size()) {
             return 0;
         }
         
-        if(prev_idx != -1 && dp[idx][prev_idx] != -1) return dp[idx][prev_idx];
+        if(dp[idx][prev_idx+1] != -1) return dp[idx][prev_idx + 1];
         
         if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
             int takeIt = 1 + create(nums, idx +1, idx, dp);
             int notPickIt = create(nums, idx +1, prev_idx, dp);
-            int mx = max(takeIt, notPickIt);
-            if(prev_idx != -1) dp[idx][prev_idx] = mx;
-            return mx;
+            
+            return dp[idx][prev_idx + 1] = max(takeIt, notPickIt);
         }
         
 
-        return dp[idx][prev_idx] = create(nums, idx +1, prev_idx, dp);
+        return dp[idx][prev_idx + 1] = create(nums, idx +1, prev_idx, dp);
     }
 };
