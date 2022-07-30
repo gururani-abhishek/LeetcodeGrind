b80746349e32598987e08affd6aa849045159a02
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-07-30 16:34:46 +0530

    Time: 1736 ms (7.26%), Space: 291.1 MB (13.93%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        //instead of passing huge prev number, using which it'll be hard to memoize the solution
+        //i'm passing, the idx of previous number.
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        
-        for(int idx=n-1; idx>=0; idx--) {
-            for(int prev_idx = idx-1; prev_idx >= -1; prev_idx--) {
-                
-            int take = 0, nottake = dp[idx+1][prev_idx+1];
-            if(prev_idx == -1 || nums[idx] > nums[prev_idx]) take = 1 + dp[idx +1][idx+1];
-
-            dp[idx][prev_idx+1] = max(take, nottake);
-            
-            }
-        }
-        
-        return dp[0][-1+1]; //+1 to prev_idx for coordinate adjustment.
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return create(nums, 0, -1, dp);
     }
     
+private: 
+    int create(vector<int>& nums, int idx, int prev_idx, vector<vector<int>>& dp) {
+        if(idx == nums.size()-1) {
+            if(prev_idx == -1 || nums[idx] > nums[prev_idx]) return 1;
+            return 0;
+        }
+        
+        if(prev_idx != -1 && dp[idx][prev_idx] != -1) return dp[idx][prev_idx];
+        
+        if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
+            int takeIt = 1 + create(nums, idx +1, idx, dp);
+            int notPickIt = create(nums, idx +1, prev_idx, dp);
+            int mx = max(takeIt, notPickIt);
+            if(prev_idx != -1) dp[idx][prev_idx] = mx;
+            return mx;
+        }
+        
+
+        return dp[idx][prev_idx] = create(nums, idx +1, prev_idx, dp);
+    }
 };
