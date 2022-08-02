f999097f2ff1cd7555998a97734a9b999cd8b19b
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-02 13:38:45 +0530

    Time: 3 ms (61.43%), Space: 6.3 MB (28.67%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n, -1);
-        return create(s, 0, dp);
-    }
-    
-private: 
-    int create(string& s, int idx, vector<int>& dp) {
-        if(idx == s.length()) return 1;
-        if(s[idx] == '0') return 0;
-    
-        if(dp[idx] != -1) return dp[idx];
-        int oneTake = create(s, idx + 1, dp);
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
         
-        int twoTake = 0;
-        if(idx + 1 < s.length()){
-            if(s[idx]=='1') twoTake = create(s, idx + 2, dp); 
-            else if(s[idx]=='2' && (s[idx+1]=='0'||s[idx+1]=='1'||s[idx+1]=='2'||s[idx+1]=='3'||s[idx+1]=='4'||s[idx+1]=='5'||s[idx+1]=='6'))
-                twoTake = create(s, idx + 2, dp);
+        for(int idx = n-1; idx >= 0; idx--) {
+            if(s[idx] == '0') {
+                dp[idx] = 0;
+                continue;
+            }
+            
+            int oneTake = dp[idx + 1];
+        
+            int twoTake = 0;
+            if(idx + 1 < s.length()){
+                if(s[idx]=='1') twoTake = dp[idx + 2]; 
+                else if(s[idx]=='2' && (s[idx+1]=='0'||s[idx+1]=='1'||s[idx+1]=='2'||s[idx+1]=='3'||s[idx+1]=='4'||s[idx+1]=='5'||s[idx+1]=='6'))
+                    twoTake = dp[idx + 2];
+            }
+
+            dp[idx] = (oneTake + twoTake);
         }
         
-        return dp[idx] = (oneTake + twoTake);
+        
+        return dp[0];
     }
 };
