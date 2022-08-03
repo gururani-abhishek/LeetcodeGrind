7dd09324c02541948165acf69c7ee390fdbcef50
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-03 09:38:30 +0530

    Time: 511 ms (45.99%), Space: 10.1 MB (48.52%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        int n = matchsticks.size(), tsum = 0;
+        for(auto u: matchsticks) tsum += u;
+        if(n < 4 || tsum % 4 != 0) return false;
+        
+        vector<int> visited(n, 0);
+        return create(matchsticks, visited, 0, 0, tsum/4, 4);
+    }
+
+private: 
+    bool create(vector<int>& matchsticks, vector<int>& visited, int idx, int sum, int target, int noOfSides) {
+        if(noOfSides == 1) return true;
+        if(idx == matchsticks.size()) return false;
+        
+        if(sum == target) return create(matchsticks, visited, 0, 0, target, noOfSides - 1);
+        
+        for(int i = idx; i < matchsticks.size(); i++) {
+            
+            if(!visited[i] && matchsticks[i] + sum <= target) {
+                visited[i] = 1;
+                if(create(matchsticks, visited, i + 1, sum + matchsticks[i], target, noOfSides)) return true;
+                visited[i] = 0;
+            }
+        }
+        
+        return false;
+    }
+};
