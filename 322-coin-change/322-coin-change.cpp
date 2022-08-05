754a0192804c54ea71c31a0f8154acd41a4d2363
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-05 12:02:16 +0530

    Time: 181 ms (32.79%), Space: 49.3 MB (14.44%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
+    //the difference between coin change and combination IV is that
+    //in coin change, the order of things do not matter(i.e. permutation)
+    //so for a testcase : {[1,2,3] 4} -> ans coin change will give = 4
+    //ans combination IV will give = 7
+    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int res = create(coins, 0, amount, dp);
-        return (res == 1e9 ? -1 : res);
-    
-    }
-    
-private: 
-    int create(vector<int>& coins, int idx, int am, vector<vector<int>>& dp) {
+        vector<vector<int>> dp(n+1, vector<int>(amount + 1, -1));
         
-        if(idx == coins.size()) {
-            return am == 0 ? 0 : 1e9;
+        for(int am = 0; am <= amount; am++) {
+            dp[n][am] = (am == 0 ? 0 : 1e9);
         }
         
-        if(dp[idx][am] != -1) return dp[idx][am];
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int am =0; am <= amount; am++) {
+                int take = (coins[idx] <= am ? 1 + dp[idx][am - coins[idx]]: 1e9);
+                int ntake = dp[idx + 1][am];
+
+                dp[idx][am] = min(take, ntake);
+            }
+        }
         
-        int take = (coins[idx] <= am ? 1 + create(coins, idx, am - coins[idx], dp) : 1e9);
-        int ntake = create(coins, idx + 1, am, dp);
-        
-        return dp[idx][am] = min(take, ntake);
+        int res = dp[0][amount];
+        return (res == 1e9 ? -1 : res);
+    
     }
 };
