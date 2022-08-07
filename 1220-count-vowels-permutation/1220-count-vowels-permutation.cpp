0f5a6eb586597cd8d71845dd39d0dcf5646f3536
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-07 17:41:55 +0530

    Time: 191 ms (23.52%), Space: 369.5 MB (5.29%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,43 +1,61 @@
 class Solution {
 public:
+    
+    //tc 
+    /*  'N' is the size of our string.
+        recursive -> exponential -> O(5*4^N-1) ~ O(4^N) ~ exponential
+        memoization -> O(N*200)
+        tabulation ->  O(N*5) {for 5 vowels}
+    */
+    //sc 
+    /*
+        recursive -> O(N) -> stack space
+        memoization -> O(N*200) + O(N) -> dp table plus stack space    
+        tabulation -> O(N*200) -> dp table
+    */
     int mod = 1e9 + 7;
     int countVowelPermutation(int n) {
         long long ans = 0;
         
-        vector<vector<int>> dp(n, vector<int>(200, -1));
+        vector<vector<long long>> dp(n+1, vector<long long>(200, 0));
         vector<char> vowels{'a', 'e', 'i', 'o', 'u'};
+        
         for(int i=0; i<5; i++) {
-            ans += create(n-1, vowels[i], dp) % mod;
+            dp[0][vowels[i]] = 1;
+        }
+        
+        for(int idx = 1; idx < n; idx++) {
+            for(auto prev: vowels) {
+                long long take = 0;
+        
+                if(prev == 'a') {
+                    take += dp[idx-1]['e'] % mod;
+                } 
+                if(prev == 'e') {
+                    take += dp[idx-1]['a'] % mod + dp[idx-1]['i'] % mod;
+                }
+                if(prev == 'i') {
+                    take += dp[idx-1]['a'] % mod + dp[idx-1]['e'] % mod + dp[idx-1]['o'] % mod + dp[idx-1]['u'] % mod;
+                }
+                if(prev == 'o') {
+                    take += dp[idx-1]['i'] % mod + dp[idx-1]['u'] % mod;
+                }
+
+                if(prev == 'u') {
+                    take += dp[idx-1]['a'] % mod;
+                }
+        
+                
+                dp[idx][prev] = take % mod;
+            }
+        }
+        
+        
+        for(int i=0; i<5; i++) {
+            ans += dp[n-1][vowels[i]] % mod;
         }
         
         return ans % mod;
     }
 
-private: 
-    long long create(int n, char prev, vector<vector<int>>& dp) {
-        if(n == 0) return 1;
-        
-        
-        if(dp[n][prev] != -1) return dp[n][prev];
-        long long take = 0;
-        
-        if(prev == 'a') {
-            take += create(n-1, 'e', dp) % mod;
-        } 
-        if(prev == 'e') {
-            take += create(n-1, 'a', dp) % mod + create(n-1, 'i', dp) % mod;
-        }
-        if(prev == 'i') {
-            take += create(n-1, 'a', dp) % mod + create(n-1, 'e', dp) % mod + create(n-1, 'o', dp) % mod + create(n-1, 'u', dp) % mod;
-        }
-        if(prev == 'o') {
-            take += create(n-1, 'i', dp) % mod + create(n-1, 'u', dp) % mod;
-        }
-        
-        if(prev == 'u') {
-            take += create(n-1, 'a', dp) % mod;
-        }
-        
-        return dp[n][prev] = take % mod;
-    }
 };
