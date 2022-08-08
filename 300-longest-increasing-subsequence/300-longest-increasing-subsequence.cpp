4af1f09d75a3c287d0d48919d7a53eed8a69eb09
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-08 20:58:32 +0530

    Time: 1826 ms (7.03%), Space: 292.3 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int idx = 0, prev_idx = -1, n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return create(nums, idx, prev_idx, dp);
-    }
-    
-private: 
-    int create(vector<int>& nums, int idx, int prev_idx, vector<vector<int>>& dp) {
-        if(idx == nums.size()) {
-            return 0;
-        }
-       
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
         
-        if(dp[idx][prev_idx + 1] != -1) return dp[idx][prev_idx +1];
-    
-        if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
-            int take = 1 + create(nums, idx+1, idx, dp); 
-            int ntake = create(nums, idx+1, prev_idx, dp);
-            
-            return dp[idx][prev_idx + 1] = max(take, ntake);
+        for(int idx = n-1; idx >= 0; idx--) {
+            for(int prev_idx = -1; prev_idx < n; prev_idx++) {
+                
+                if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
+                    int take = 1 + dp[idx+1][idx+1];
+                    int ntake = dp[idx+1][prev_idx+1];
+
+                    dp[idx][prev_idx + 1] = max(take, ntake);
+                    continue;
+                }
+
+                dp[idx][prev_idx + 1] = dp[idx + 1][prev_idx+1];
+
+            }
         }
         
-        return dp[idx][prev_idx + 1] = create(nums, idx + 1, prev_idx, dp);
+        
+        
+        return dp[0][-1+1];
     }
+    
 };
