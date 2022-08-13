709596a75f48e737113e5f41a83600746d2a1963
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-13 23:04:38 +0530

    Time: 4 ms (91.26%), Space: 8.3 MB (76.34%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,55 +1,38 @@
 class Solution {
 public:
     
-    // bruteforce backtracking = tc : o(2n), sc : o(n) -> stack space.
-    // memoized = tc : o(n3), sc : o(n) + o(n)-> stack space & dp array ;
-    //tc : o(n3), outerloop(add_idx -> worstcase o(n)), inner recursion(o(n) worstcase), findOperation(o(n));
-    
+    // recursive -> tc : O(2^n) -> I can either divide or not(2choices) the given string into 'n' pieces, sc : O(n)
+    // memoization -> tc : O(n*n*n) -> (n calls * n sized loop * n find) 
     bool wordBreak(string s, vector<string>& wordDict) {
-        set<string> mapWordDict(wordDict.begin(), wordDict.end());
         
-        int n = s.length();
-        vector<int> dp(n+1, 0);
+        //on every call if the first part is matched, I've to search for the rest part
+        //to make search easy, I'm using a set. find tc : O(n) -> {worst case}
+        unordered_set<string> wd(wordDict.begin(), wordDict.end());
+        vector<int> dp(s.length(), -1);
+        return create(s, wd, 0, dp);
+    }
+    
+private: 
+    bool create(string &s, unordered_set<string> &wordDict, int idx, vector<int> &dp) {
+        //if idx has reached the end, our string is already divided into space separated substrings 
+        //present in wordDict
+        if(idx == s.length()) return true;
         
-        dp[n] = 1;
         
-        for(int idx = n-1; idx >=0; idx--) {
-             string sub = "";
-
-            for(int add_idx = idx; add_idx < n; add_idx++) {
-                sub += s[add_idx];
-                if(mapWordDict.find(sub) != mapWordDict.end() && dp[add_idx+1])  {
-                dp[idx] = true;
-                break;
-                }
-            }
+        if(dp[idx] != -1) return dp[idx];
+        
+        //in every call I've to create a new substring starting from idx
+        string sub = ""; //new call, hence a new substring to be made
+        for(int i = idx; i < s.length(); i++) {
+            sub += s[i];//keep on adding the string chars
+            
+            //check if the substring is present or not, if present carry on the same process for the leftover part
+            if(wordDict.find(sub) != wordDict.end() && create(s, wordDict, i+1, dp)) 
+                return dp[idx] = true;
         }
         
         
-        
-        return dp[0];
+        //didn't match -> return false
+        return dp[idx] = false;
     }
-
 };
-
-/*
-
-
-case : applepenapple
-
-ans = true;
-
-create(idx = 0) -> return true;
-(apple -> found) {
-    create(find pen);  -> return true;
-}    
-
-(pen -> found) {
-    create(find apple); -> return true;
-}
-
-(apple -> found) {
-    create(rest of the part, that is end of the string); -> return true;
-}
-     
-*/
