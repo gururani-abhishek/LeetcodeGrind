6be13f657e572e59cee6385bb6f521b41a0434f5
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-17 15:09:06 +0530

    Time: 11 ms (26.40%), Space: 12.2 MB (36.78%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        //hahahahahahahha
+        
+        next_permutation(nums.begin(), nums.end());
+        
+        //hahahahahahahah
+    }
+};
