21d51d6929943d37148c94f7ee0581de3cb0ce56
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-17 16:10:02 +0530

    Time: 4 ms (77.27%), Space: 12.1 MB (36.78%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,10 +1,46 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //hahahahahahahha
+        int n = nums.size();
         
-        next_permutation(nums.begin(), nums.end());
+        int idx1 = INT_MAX; //step 1 element
         
-        //hahahahahahahah
+        for(int idx=0; idx<n-1; idx++) {
+           if(nums[idx] < nums[idx + 1]) {
+               idx1 = idx;
+           }
+        } 
+        
+        //if the vector is last dict perm(i.e. descending order sorted) then you have to return 
+        //vector sorted in ascending order, or reverse of the given vector
+        if(idx1 == INT_MAX) {
+            reverseSeg(nums, 0);
+            return;
+        }
+        
+        int idx2; //step 2 element
+        for(int idx = n-1; idx > idx1; idx--) {
+            if(nums[idx] > nums[idx1]) {
+                idx2 = idx;
+                break;
+            }
+        }
+        
+        swap(nums[idx1], nums[idx2]); //swap step1 and step2 elements
+        
+        //reverse segement that's to the right of idx1
+        reverseSeg(nums, idx1 + 1);        
+        
+    }
+    
+private: 
+    
+    void reverseSeg(vector<int> &nums, int idx1) {
+        int lo = idx1, hi = nums.size() -1;
+        
+        while(lo < hi) {
+            swap(nums[lo], nums[hi]);
+            lo++, hi--;
+        }
     }
 };
