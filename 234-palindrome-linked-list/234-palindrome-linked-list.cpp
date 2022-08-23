aa6ef40ecddbdd70f37a14569e3f0e1d0be0bd13
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-23 23:45:23 +0530

    Time: 405 ms (40.06%), Space: 114.1 MB (74.71%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,28 +10,58 @@
  */
 class Solution {
 public:
-    // tc : O(n), space : O(n)
+
     bool isPalindrome(ListNode* head) {
-        if(head == NULL) return false;
+        ListNode* endOfFirstHalf = findMiddle(head);
+        ListNode* right = reverseSegment(endOfFirstHalf);
+        ListNode* left = head;
         
-        vector<int> arr;
-        
-        while(head != NULL) {
-            arr.push_back(head -> val);
-            head = head -> next;
-        }
-        
-        return checkPali(arr);
-    }
-    
-private: 
-    bool checkPali(vector<int> &arr) {
-        int left = 0, right = arr.size() -1;
-        while(left < right) {
-            if(arr[left] != arr[right]) return false;
-            left++, right--;
+        while(right != nullptr) {
+            if(left -> val != right -> val) return false;
+            left = left -> next;
+            right = right -> next;
         }
         
         return true;
+    }
+    
+private: 
+    //head, will point to the reverse of the array    
+    ListNode* reverseSegment(ListNode* head) {
+        ListNode* dummy = nullptr;
+        ListNode* next = nullptr;
+        
+        while(head != nullptr) {
+            next = head -> next;
+            head -> next = dummy;
+            dummy = head;
+            head = next;
+        }
+        
+        return dummy;
+        
+    }
+    
+    // even elements -> returns pointer to the first element of second half
+    // odd elements -> returns pointer to the middle element
+    
+    // in all cases we need the other half, so we'll tweak a bit.
+    ListNode* findMiddle(ListNode* head) {
+        
+        ListNode* slow = head; //will move 1 step
+        ListNode* fast = head; //will move 2 step
+        
+        while(fast != nullptr && fast -> next != nullptr) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        
+        //for even it's already giving a pointer to the end of first half
+        //but for odd, it's the middle element, so incrementing it to the next node, 
+        // which will be next to the middle element.
+        
+        if(fast != nullptr) slow = slow -> next;
+        return slow;
+        
     }
 };
