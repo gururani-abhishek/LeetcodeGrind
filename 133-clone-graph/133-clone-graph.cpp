630dede0f3fd23efa4fd49686be8aeedb91a2edf
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-25 23:26:04 +0530

    Time: 15 ms (19.15%), Space: 8.8 MB (66.47%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -19,36 +19,38 @@ public:
 };
 */
 
-// what is the  difference beetween copy and clone?
 class Solution {
 public:
-    // I'll traverse my original node, and every node I'll create a newClone node,  with same value  
-    // and same neighbors. 
-    
     Node* cloneGraph(Node* node) {
-        if(node  == nullptr) return node;
+        if(node == nullptr) return node;
         
-        unordered_map<Node*, Node*> oldNew; // it contains new cloned nodes for old nodes.
-        
-        clone(node, oldNew);
-        return oldNew[node]; 
+        unordered_map<Node*, Node*> oldNew;
+        return clone(node, oldNew);
     }
+
 private: 
     Node* clone(Node* node, unordered_map<Node*, Node*> &oldNew) {
-        // if its clone is already created : 
-        if(oldNew.find(node) != oldNew.end()) return oldNew[node];
         
-        // if here it means that the clone isn't created : 
-        // oldNew[node] = new Node(node -> val); // clone with same value added. connection missing.
-        Node* clonedNode = new Node(node -> val);
-        oldNew[node] = clonedNode;
-        for(auto adjNode : node -> neighbors) {
-                // (oldNew[node] -> neighbors).push_back(clone(adjNode, oldNew));
-                (clonedNode -> neighbors).push_back(clone(adjNode, oldNew));
-        }
+            Node* clonedNode = new Node(node -> val);
+            oldNew[node] = clonedNode; // I've mapped old node with new clonedNode
+        // this will be used further when I'll hop on to this node again.
+            
+            // explore all the neighbors :
+            for(auto adjNode : node -> neighbors) {
+                // if this neighbor has already been cloned, find it in oldNew
+                // and push it in the neighbors list in clonedNode
+                
+                if(oldNew.find(adjNode) != oldNew.end()) {
+                    (clonedNode -> neighbors).push_back(oldNew[adjNode]);
+                } else {
+                    // if not in newOld, then call the recursive function
+                    // where you'll create the new clone and then pass it back
+                    (clonedNode -> neighbors).push_back(clone(adjNode, oldNew));
+                } 
+            }
+        // if here it means that this clonedNode is now made.
+        // Just return it for the previous recursion call : 
         
-        // return oldNew[node];
         return clonedNode;
-        
     }
 };
