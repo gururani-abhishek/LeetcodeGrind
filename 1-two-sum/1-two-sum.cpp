84fbac34f8d9777612aff99db38d3db41a9c219c
Abhishek Gururani <59947871+gururani-abhishek@users.noreply.github.com>
2022-08-27 18:16:37 +0530

    Time: 15 ms (80.90%), Space: 10.9 MB (47.75%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    // you have to return the indices, and not the values.
-    // I need a vector of pair, to store the indices too.
     vector<int> twoSum(vector<int>& nums, int target) {
-        // need to store elements and their indices in a vector pair, 
-        vector<pair<int, int>> numsPlusIdx; 
+        unordered_map<int, int> mapValIdx;  // mapping val with it's idx
+        
+        vector<int> res; // will store the result
+        
+        // when we use a hash map, our focus should be on searching for it's elements : 
         for(int idx = 0; idx < nums.size(); idx++) {
-            // as I'll sort the elements on the basis of their values, 
-            // the first pair is going to be the element. 
-            numsPlusIdx.push_back({nums[idx], idx});
-        }
+            
+            int otherElement = target - nums[idx]; // now I'll search for this otherElement
         
-        // a vector pair gets sorted on the basis of 1st value(i.e. elements)
-        sort(numsPlusIdx.begin(), numsPlusIdx.end()); 
-        
-        // 1, 2, 3, 4, 5, 6
-        
-        vector<int> res;
-        int l = 0, r = nums.size() - 1; // two pointers, left and right
-        while(l < r) {
-            if(numsPlusIdx[l].first + numsPlusIdx[r].first > target) {
-                r--;
-            } else if(numsPlusIdx[l].first + numsPlusIdx[r].first < target) {
-                l++;
-            } else {
-                res = {numsPlusIdx[l].second, numsPlusIdx[r].second};
+            // map.begin()         map.end()
+            // [-] [-] [-] [-] [-] 
+            
+            
+            if(mapValIdx.find(otherElement) != mapValIdx.end()) {
+                // other element exists, I found pairs that make up target : 
+                res = {idx, mapValIdx[otherElement]};
                 break;
             }
+            
+            // map them, val ---> idx, for further checks : 
+            mapValIdx[nums[idx]] = idx;
         }
-        
+    
         return res;
     }
 };
+
+// I need to find two elements whose sum equals target
+// suppose one of those two element is 'x', then the other element will be : 'target - x'
+// after this observation, this question boils down to finding 'target-x' 
+// for every single element of the array, and when found then return their indices.
